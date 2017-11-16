ca71856a60b38593dbe956a5f7934c7500ab1422
ssrinivasa <ssrinivasa@paypal.com>
2017-11-15 21:43:22 -0600

    Intial check in of springboot app

---
+++ src/main/java/com/springboot/example/ApplicationJava.java
@@ -0,0 +1,15 @@
+package com.springboot.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by ssrinivasa on 11/15/17.
+ */
+@SpringBootApplication
+public class ApplicationJava {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationJava.class,args);
+    }
+}
